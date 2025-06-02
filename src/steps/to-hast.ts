e7857dc3877189ac75f492f9c716312c3ca18c5f
Melissa Garcia <ens15038@adobe.com>
2025-06-02 11:19:34 -0700

    Revert "feat: pre code attributes"
    This reverts commit 80f5e3fdfee8dd21268be16d1d2f9b70fc4b9c6a.

--- src/steps/to-hast.ts
+++ src/steps/to-hast.ts
@@ -19,41 +19,12 @@ import { Helix } from '@adobe/helix-universal';
 import { toHast as mdast2hast, defaultHandlers, State } from 'mdast-util-to-hast';
 import { raw } from 'hast-util-raw';
 import { mdast2hastGridTablesHandler, TYPE_TABLE } from '@adobe/mdast-util-gridtables';
-import { Code, Element } from 'mdast-util-to-hast/lib/handlers/code.js';
-
-const customHandlers = {
-
-  code(state: State, node: Code): Element | undefined {
-    // Call the default handler for 'code' to get the standard <pre><code> structure.
-    // This handles setting the 'language-xyz' class e.g. <pre><code class="language-xyz">
-    const pre = defaultHandlers.code(state, node);
-
-    // Parse the 'meta' string into a properties object.
-    // This handles setting attributes e.g. <pre data-line="1-2, 5, 9-20"><code>
-    const keyValuePairs = Array.from(node.meta.matchAll(/(\S*)(\s*=\s*")(.*?)(")/g), (match) => [match[1], match[3]]);
-    const metaProperties = Object.fromEntries(keyValuePairs);
-
-    if (pre && pre.type === 'element' && pre.tagName === 'pre') {
-      pre.properties = {
-        ...pre.properties,
-        ...metaProperties,
-      };
-    }
-
-    return pre;
-  },
-
-  // No other handlers are defined here.
-  // This means 'paragraph', 'heading', 'list', 'listItem', 'text', etc.,
-  // will all be processed by `mdast-util-to-hast`'s built-in `defaultHandlers`.
-
-};
 
 export default function toHast(ctx: Helix.UniversalContext) {
   const { content } = ctx.attributes;
   content.hast = mdast2hast(content.mdast, {
     handlers: {
-      ...customHandlers,
+      ...defaultHandlers,
       section: (state: State, node: any) => {
         const n = { ...node };
         const children = state.all(n);
